4d4905344f81524f0541931a4acb319fe6d9e24f
Admin <Admin@Asus>
2016-11-10 12:16:42 +0100

    esercizio 1

---
+++ esercizio1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        print "Twinkle, Twinkle little star.<br>";
+        $a="Twinkle";
+        $b="Star";
+        echo "$a, $a little $b<br>";
+        $a = "Fuck you";
+        $b = "Trump";
+        echo "$a, $a little $b<br>";
+        $x=10;
+        $y=7;
+        $z=10+7;
+        echo "10+7=$z";
+        ?>
+    </body>
+</html>
